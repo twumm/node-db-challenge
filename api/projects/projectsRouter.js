ddab114982dd3e57fd08b536e0f6a5fe45914bab
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-19 09:38:58 +0000

    feature: Add custom middleware

--- api/projects/projectsRouter.js
+++ api/projects/projectsRouter.js
@@ -13,7 +13,7 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-router.get('/:id', async (req, res, next) => {
+router.get('/:id', validateProjectId, async (req, res, next) => {
   const { id } = req.params;
   try {
     const project = await projectsDb.getProject(id);
@@ -23,7 +23,7 @@ router.get('/:id', async (req, res, next) => {
   }
 })
 
-router.post('/', async (req, res, next) => {
+router.post('/', validationProjectContent, async (req, res, next) => {
   const project = { name, description } = req.body;
   try {
     const newProject = await projectsDb.addProject(project);
@@ -33,7 +33,7 @@ router.post('/', async (req, res, next) => {
   }
 })
 
-router.post('/:id/action', async (req, res, next) => {
+router.post('/:id/action', [validateProjectId, validationActionContent] async (req, res, next) => {
   const { id } = req.params;
   const action = { description, notes } = req.body;
   try {
@@ -43,5 +43,40 @@ router.post('/:id/action', async (req, res, next) => {
     next(new Error('Cannot create action'));
   }
 })
+
+// custom middlewares
+async function validateProjectId(req, res, next) {
+  const { id } = req.params;
+  if (isNaN(Number(id))) {
+    res.status(400).json({ message: 'User id must be a number' })
+  }
+  const project = await projectDb.get(id);
+  if (project) {
+    req.project = project;
+    next();
+  } else {
+    res.status(400).json({ message: "Invalid project id" });
+  }
+}
+
+function validationProjectContent(req, res, next) {
+  if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+    res.status(400).json({ message: "Missing project data" });
+  } else if (!req.body.name || !req.body.description) {
+    res.status(400).json({ message: "Missing required *name* and *description* fields" });
+  } else {
+    next();
+  }
+}
+
+function validationActionContent(req, res, next) {
+  if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+    res.status(400).json({ message: "Missing action data" });
+  } else if (!req.body.description || !req.body.notes) {
+    res.status(400).json({ message: "Missing required *description* and *notes* fields" });
+  } else {
+    next();
+  }
+}
 
 module.exports = router;
