787bb39b6d3619bc43fd2fae5e1f7484715bf3da
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-19 09:50:33 +0000

    feature: Add methods for actions

---
+++ api/actions/actionsRouter.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const router = express.Router();
+const actionsDb = require('./actionsDb');
+
+router.get('/', async (req, res, next) => {
+  try {
+    const actions = await actionsDb.getAllActions();
+    res.status(200).json(actions);
+  } catch (error) {
+    next(new Error('Cannot get actions'));
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const action = await actionsDb.getAction(id);
+    res.status(200).json(action);
+  } catch (error) {
+    next(new Error('Cannot get action'));
+  }
+})
+
+// custom middlewares
+async function validateActionId(req, res, next) {
+  const { id } = req.params;
+  const action = await actionsDb.get(id);
+  if (action) {
+    req.action = action;
+    next()
+  } else {
+    next(new Error('Invalid action id'))
+  }
+}
+
+module.exports = router;
