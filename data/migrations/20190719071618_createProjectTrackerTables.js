a8442e17bc28f32bd6f79008c34c1bacff96bf9f
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-19 10:39:16 +0000

    feature: Add tables for context and action context

--- data/migrations/20190719071618_createProjectTrackerTables.js
+++ data/migrations/20190719071618_createProjectTrackerTables.js
@@ -1,5 +1,5 @@
 
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema
     .createTable('projects', table => {
       table.increments();
@@ -24,10 +24,34 @@ exports.up = function(knex) {
         .onUpdate('CASCADE')
         .onDelete('CASCADE');
     })
+    .createTable('contexts', table => {
+      table.increments();
+      table.text('context')
+        .notNullable();
+    })
+    .createTable('action_contexts', table => {
+      table.increments();
+      table.integer('action_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('actions')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      table.integer('context_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('context')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+    })
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema
+    .dropTableIfExists('action_contexts')
+    .dropTableIfExists('context')
     .dropTableIfExists('actions')
     .dropTableIfExists('projects')
 };
